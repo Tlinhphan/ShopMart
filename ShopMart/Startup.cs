8ebc0df6af2468eee5975b118663dd39e4c5ef04
Tlinhphan <tlinhphan201@gmail.com>
2025-12-02 01:03:07 +0700

    lesson16

--- ShopMart/Startup.cs
+++ ShopMart/Startup.cs
@@ -107,6 +107,10 @@ namespace ShopMart
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
+
+
+                routes.MapRoute(name: "areaRoute",
+                    template: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
             });
 
            
