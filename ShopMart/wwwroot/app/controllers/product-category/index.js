95b71c2662dc4a9b312c1656fd0520228e8d6f61
Tlinhphan <tlinhphan201@gmail.com>
2026-01-05 22:03:42 +0700

    Sort and update parent by drag and drop

--- ShopMart/wwwroot/app/controllers/product-category/index.js
+++ ShopMart/wwwroot/app/controllers/product-category/index.js
@@ -4,7 +4,7 @@
     }
 
     function loadData() {
-  
+
         $.ajax({
             url: '/Admin/ProductCategory/GetAll',
             dataType: 'json',
@@ -18,15 +18,67 @@
                         sortOrder: item.SortOrder
                     });
                 });
-         
                 var treeArr = shopmart.unflattern(data);
+                treeArr.sort(function (a, b) {
+                 return a.SortOrder - b.SortOrder;
+                 });
 
-                //var $tree = $('#treeProductCategory');
-                $('#treeProductCategory').tree({
-                    data: treeArr,
-                    dnd: true
-                });
+
+        var treeArr = shopmart.unflattern(data);
+
+        //var $tree = $('#treeProductCategory');
+        $('#treeProductCategory').tree({
+            data: treeArr,
+            dnd: true,
+            onDrop: function (target, source, point) {
+                console.log(target);
+                console.log(source);
+                console.log(point);
+                var targetNode = $(this).tree('getNode', target);
+                if (point == 'append') {
+                    var children = [];
+                    $.each(targetNode.children, function (i, item) {
+                        children.push({
+                            key: item.id,
+                            value: i
+                        });
+                    });
+
+                    //Update to database
+                    $.ajax({
+                        url: '/Admin/ProductCategory/UpdateParentId',
+                        type: 'post',
+                        dataType: 'json',
+                        data: {
+                            sourceId: source.id,
+                            targetId: targetNode.id,
+                            items: children
+                        },
+
+                        success: function (res) {
+                            loadData();
+                        }
+                    });
+                }
+                else if (point === 'top' || point === 'bottom') {
+                    $.ajax({
+                        url: '/Admin/ProductCategory/ReOrder',
+                        type: 'post',
+                        dataType: 'json',
+                        data: {
+                            sourceId: source.id,
+                            targetId: targetNode.id
+                        },
+
+                        success: function (res) {
+                            loadData();
+                        }
+                    });
+                }
             }
-        })
+
+        });
+    }
+});
     }
 }
