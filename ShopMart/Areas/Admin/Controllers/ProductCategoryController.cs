1c37b1965340516a27159fbc47b1a12f2f129216
Tlinhphan <tlinhphan201@gmail.com>
2026-01-06 23:19:05 +0700

    lesson28

--- ShopMart/Areas/Admin/Controllers/ProductCategoryController.cs
+++ ShopMart/Areas/Admin/Controllers/ProductCategoryController.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using ShopMart.Application.Interfaces;
+using ShopMart.Application.ViewModels.Product;
+using ShopMart.Utilities.Helpers;
 
 namespace ShopMart.Areas.Admin.Controllers
 {
@@ -22,6 +25,55 @@ namespace ShopMart.Areas.Admin.Controllers
         }
 
         #region Get Data API
+        [HttpGet]
+        public IActionResult GetById(int id)
+        {
+            var model = _productCategoryService.GetById(id);
+
+            return new ObjectResult(model);
+
+        }
+        [HttpPost]
+        public IActionResult SaveEntity(ProductCategoryViewModel productVm)
+        {
+            if (!ModelState.IsValid)
+            {
+                IEnumerable<ModelError> allErrors = ModelState.Values.SelectMany(v => v.Errors);
+                return new BadRequestObjectResult(allErrors);
+            }
+            else
+            {
+                productVm.SeoAlias = TextHelper.ToUnsignString(productVm.Name);
+                if (productVm.Id == 0)
+                {
+                    _productCategoryService.Add(productVm);
+                }
+                else
+                {
+                    _productCategoryService.Update(productVm);
+                }
+                _productCategoryService.Save();
+                return new OkObjectResult(productVm);
+
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            if (id == 0)
+            {
+                return new BadRequestResult();
+            }
+            else
+            {
+                _productCategoryService.Delete(id);
+                _productCategoryService.Save();
+                return new OkObjectResult(id);
+            }
+
+        }
+
         [HttpGet]
         public IActionResult GetAll()
         {
