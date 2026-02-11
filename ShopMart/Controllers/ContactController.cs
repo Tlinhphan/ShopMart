5b3d2ff83c2c2980b269979aa4fd50f1a73894e6
Tlinhphan <tlinhphan201@gmail.com>
2026-02-11 14:56:02 +0700

    lesson43

---
+++ ShopMart/Controllers/ContactController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ShopMart.Controllers
+{
+    public class ContactController : Controller
+    {
+        [Route("contact.html")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
